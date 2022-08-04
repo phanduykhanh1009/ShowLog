cc7b2fb46e816e9ef5a84d603d5ed32a433a1b9e
phanduykhanh1009 <phanduykhanh1009@gmail.com>
2022-08-04 10:44:03 +0700

    no message

---
+++ LogService.swift
@@ -0,0 +1,15 @@
+//
+//  LogService.swift
+//  ShowLog
+//
+//  Created by KhanhPD2.APL on 04/08/2022.
+//
+
+import Foundation
+
+open class LogService {
+    public static let shared = LogService()
+    public func logDemo() {
+        print("KhanhPD2 Hello")
+    }
+}
